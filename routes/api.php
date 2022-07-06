09ad51c6f44797fd11cb86c60c3d81abf0f6eefe
UsmanSiddique1 <usmansiddique406@gmail.com>
2022-07-06 18:57:18 +0500

    endpoints

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,34 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('leads', function(){
+    $leads = Lead::all();
+    return response()->json($leads);
+});
+
+Route::get('leads\{lead}', function(Lead $lead){
+    return response()->json($lead);
+});
+
+Route::post('lead', function(Request $request){
+    $lead = Lead::create($request);
+    return response()->json($lead, 201);
+});
+
+Route::get('appointments', function(){
+    $appointments = Appointment::all();
+    return response()->json($appointments);
+});
+
+Route::get('appointment\{appointment}', function(Lead $appointment){
+    return response()->json($appointment);
+});
+
+Route::post('appointment', function(Request $request){
+    $appointment = Appointment::create($request);
+    return response()->json($appointment, 201);
+});
+
+
+
